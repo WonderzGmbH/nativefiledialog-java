f3207205ccd6a0ca7d68842f046e35db0d01dfd6
Artem Chepurnoy <artemchep@gmail.com>
2023-06-26 20:22:40 +0300

    Add maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.32"
+    id("maven-publish")
+    id("signing")
 }
 
 group = "tv.wunderbox"
@@ -17,6 +19,25 @@ kotlin {
 
 dependencies {
     implementation("net.java.dev.jna:jna:5.13.0")
+}
+
+// See:
+// https://www.jetbrains.com/help/space/publish-artifacts-to-maven-central.html
+// on how to publish to maven repos.
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = "nativefiledialog"
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
 }
 
 tasks.withType<KotlinCompile>() {
