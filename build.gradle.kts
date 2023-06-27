e94cb9fbfe5ce2226001dbe2d498d6878bc59edb
Artem Chepurnoy <artemchep@gmail.com>
2023-06-27 16:51:47 +0300

    Add publish to maven pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,15 @@ plugins {
 }
 
 group = "tv.wunderbox"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
 
 kotlin {
@@ -32,9 +37,49 @@ publishing {
             version = project.version.toString()
 
             from(components["java"])
+
+            pom {
+                packaging = "jar"
+                name.set("Native File Dialog Java")
+                url.set("https://github.com/WonderzGmbH/nativefiledialog-java")
+                description.set("A wrapper around the small btzy/nativefiledialog-extended. As a fallback it uses AWT file picker.")
+
+                licenses {
+                    license {
+                        name.set("zlib License")
+                        url.set("https://github.com/WonderzGmbH/nativefiledialog-java/blob/master/LICENSE")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:https://github.com/WonderzGmbH/nativefiledialog-java.git")
+                    developerConnection.set("scm:git@github.com:WonderzGmbH/nativefiledialog-java.git")
+                    url.set("https://github.com/WonderzGmbH/nativefiledialog-java")
+                }
+
+                developers {
+                    developer {
+                        id.set("AChep")
+                        name.set("Artem Chepurnyi")
+                        email.set("artemchep@gmail.com")
+                    }
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsUrl else releasesUrl
+            credentials {
+                username = project.properties["ossrhUsername"].toString()
+                password = project.properties["ossrhPassword"].toString()
+            }
         }
     }
 }
+
 
 signing {
     sign(publishing.publications)
