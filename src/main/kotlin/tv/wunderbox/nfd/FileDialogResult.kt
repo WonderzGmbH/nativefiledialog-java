40539e4a94935e8e38c932640269391c10d7948d
Russell Banks <74878137+russellbanks@users.noreply.github.com>
2023-07-03 11:23:04 +0100

    Add FileDialogResult.isSuccess

--- src/main/kotlin/tv/wunderbox/nfd/FileDialogResult.kt
+++ src/main/kotlin/tv/wunderbox/nfd/FileDialogResult.kt
@@ -1,6 +1,12 @@
 package tv.wunderbox.nfd
 
 public sealed interface FileDialogResult<out T> {
+    /**
+     * Returns `true` if this instance represents a successful outcome.
+     * In this case [isFailure] returns `false`.
+     */
+    public val isSuccess: Boolean get() = this !is Failure
+
     public data class Failure(
         val error: FileDialog.Error,
     ) : FileDialogResult<Nothing>
